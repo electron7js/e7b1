0bfff663dbc98b3da5289245e9d8cf956cadada3
electron7js <96103384+electron7js@users.noreply.github.com>
2022-04-25 11:43:39 +0545

    title changed

--- pages/index.js
+++ pages/index.js
@@ -6,7 +6,7 @@ export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Next.js Starter!</title>
+        <title>electron7's rickroll</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
